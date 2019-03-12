468c1c3a2230833b7436243f5bec10ae878807a5
jim huang <jim.javathunderbird@gmail.com>
2019-03-12 12:05:20 -0700

    search class then search function

--- src/Service/Search.php
+++ src/Service/Search.php
@@ -18,6 +18,7 @@ class Search extends BaseService
         $contextLine = $lines[$line - 1];
         $contextPosition = $this->options['pos'];
 
+        //first, try to see if this is a possible class
         $contextLine = str_replace('::',' ', $contextLine);
 
         $leftPart = substr($contextLine, 0, $contextPosition - 1);
@@ -50,6 +51,39 @@ class Search extends BaseService
                     if (strpos($fileInfo[0], $word) !== FALSE) {
                         $result .= "{$fileInfo[0]}({$fileInfo[1]})\n";
                     }
+                }
+            }
+        }
+
+        $resultLength = strlen(trim($result));
+        if ($resultLength === 0) {
+            //now there is no match on the classes, try to find the functions
+            //look to the left
+            $wordLeftPos = $contextPosition - 1;
+            for ($i = $wordLeftPos; $i >= 0; $i--) {
+                if (in_array($contextLine[$i], ['>',':',' '])) {
+                    $wordLeftPos = $i + 1;
+                    break;
+                }
+            }
+            //look to the right
+            $contextLineLength = strlen($contextLine);
+            $wordRightPos = $contextPosition;
+            for ($i = $wordRightPos; $i < $contextLineLength; $i++) {
+                if (in_array($contextLine[$i], ['>',':',' ','(',')'])) {
+                    $wordRightPos = $i - 1;
+                    break;
+                }
+            }
+            $word = substr($contextLine, $wordLeftPos, $contextPosition - $wordLeftPos)
+                .substr($contextLine, $contextPosition, $wordRightPos - $contextPosition + 1);
+
+            $projectIndex = $project->getIndex();
+            $functionsIndex = $projectIndex['functions'];
+            if (isset($functionsIndex[$word])) {
+                $fileInfos = $functionsIndex[$word];
+                foreach($fileInfos as $fileInfo) {
+                    $result .= "{$fileInfo[0]}({$fileInfo[1]})\n";
                 }
             }
         }
