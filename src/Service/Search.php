761ada6a451afd4ac9e3bfedc46a2f7575e809ad
jim huang <jim.javathunderbird@gmail.com>
2019-03-16 23:49:56 -0700

    detect left (

--- src/Service/Search.php
+++ src/Service/Search.php
@@ -33,7 +33,7 @@ class Search extends BaseService
     }
 
     private function getWordFromLineAndPosition($contextLine, $contextPosition) {
-        $leftStoppingSymbolsHash = ['>' => 1, ':' => 1, ',' => 1, ';' => 1,' ' => 1,'[' => 1];
+        $leftStoppingSymbolsHash = ['>' => 1, ':' => 1, ',' => 1, ';' => 1,' ' => 1,'[' => 1,'(' => 1];
         $rightStoppingSymbolsHash = ['>' => 1,':' => 1,' ' => 1,'(' => 1,')' => 1,';' => 1,',' => 1,'{' => 1,']' => 1];
 
         //look to the left
