3f177ef4586e097f65a6a5f912c425fad764bc45
jim huang <jim.javathunderbird@gmail.com>
2019-04-06 22:40:06 -0700

    handle simple class method case

--- src/Util/WordSearchEngine.php
+++ src/Util/WordSearchEngine.php
@@ -5,16 +5,41 @@ class WordSearchEngine
 {
     private $projectHash;
     private $dataRoot;
+    private $dataDir;
 
     public function __construct($projectHash, $dataRoot) {
         $this->projectHash = $projectHash;
         $this->dataRoot = $dataRoot;
+        $this->dataDir = $this->dataRoot.'/'.$this->projectHash;
     }
 
     public function doSearch($file, $contextLine, $contextPosition) {
+        $possibleFileInfos = [];
+
         $word = $this->getWordFromLineAndPosition($contextLine, $contextPosition); 
-        $dataDir = $this->dataRoot.'/'.$this->projectHash;
-        $result = [];
+
+        //pattern: class::method
+        $wordSplits = explode("::", $word);
+        $wordSplitsCount = count($wordSplits);
+        if ($wordSplitsCount === 2) {
+            $classPath = $wordSplits[0]; 
+            $classPathSplits = explode("/", $classPath);
+            $className = array_pop($classPathSplits);
+            $classIndex = $this->dataDir."/class.$className.index";
+            if (file_exists($classIndex)) {
+                $fileInfos = explode("\n",trim(file_get_contents($classIndex)));
+                foreach($fileInfos as $fileInfo) {
+                    $file = explode(":", $fileInfo)[0];
+                    if (strpos($file, $classPath) !== FALSE) {
+                        $possibleFileInfos[] = $fileInfo;
+                    }
+                }
+            }
+        }
+
+        $result = $this->getResultFromFileInfos($possibleFileInfos);
+
+        return $result;
     }
 
     private function getWordFromLineAndPosition($contextLine, $contextPosition) {
@@ -49,5 +74,18 @@ class WordSearchEngine
         $word = str_replace("\\", "/", $word);
 
         return $word;
+    }
+
+    private function getResultFromFileInfos($possibleFileInfos) {
+        $result = "";
+        $lineNum = 0;
+        foreach($possibleFileInfos as $fileInfo) {
+            $lineNum ++;
+            $comps = explode(":", $fileInfo);
+            $filePath = $comps[0];
+            $line = $comps[1];
+            $result .= "$lineNum. {$filePath}({$line})\n";
+        }
+        return $result;
     }
 }
