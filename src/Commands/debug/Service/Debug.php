036c919e96e4d8b959bdee2d9350f869c8b7db8f
jimthunderbird <jim.javathunderbird@gmail.com>
2019-05-14 09:39:26 -0700

    fix comments

--- src/Commands/debug/Service/Debug.php
+++ src/Commands/debug/Service/Debug.php
@@ -25,7 +25,7 @@ class Debug extends BaseService
             $this->cleanBreakpoints($sourceFile);
             print "Error adding breakpoint, syntax error\n";
         } else {
-            //run the cmd and forget about the output
+            //run the cmd
             echo shell_exec($this->cmd);
             //clean up the break points again
             $this->cleanBreakpoints($sourceFile);
