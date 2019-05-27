0f6917e78ed531d6af102e534b56bcbfe7ebdbd1
Jim Huang <jim.huang@glu.com>
2019-05-27 12:09:06 -0700

    create index table structure for phim project indexes

--- sqls/create_phim_mysql_user_and_db.sql
+++ sqls/create_phim_mysql_user_and_db.sql
@@ -3,3 +3,12 @@ CREATE USER 'phimide'@'localhost' IDENTIFIED WITH mysql_native_password BY '1234
 GRANT ALL PRIVILEGES ON *.* TO 'phimide'@'localhost';
 DROP DATABASE IF EXISTS phim_ide_db;
 CREATE DATABASE phim_ide_db;
+USE phim_ide_db;
+CREATE TABLE phim_ide_project_index (
+    project_hash varchar(32) primary key,
+    index_type varchar(10), 
+    index_name varchar(255),
+    index_info text,
+    key(index_type),
+    key(index_name)
+);
