b98b68098466d87c390607d4cac5456d94d3ac17
Jim Huang <jim.huang@glu.com>
2019-05-27 10:23:17 -0700

    add sql to create phim mysql user and db

--- config/config.php
+++ config/config.php
@@ -1,4 +1,11 @@
 <?php
 return [
     'dataRoot' => "/usr/local/phim/projects",
+    'db' => [
+        'type' => 'mysql',
+        'host' => 'localhost',
+        'user' => 'phimide',
+        'pass' => '123456',
+        'db' => 'phim_ide_db'
+    ]
 ];
