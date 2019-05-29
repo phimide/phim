da3ac4738dc7c037610ec50158031d0c9567f955
Jim Huang <jim.huang@glu.com>
2019-05-28 21:02:25 -0700

    fix file listing, deprecated project parser

--- src/Commands/project.listfiles/Service/ListFiles.php
+++ src/Commands/project.listfiles/Service/ListFiles.php
@@ -1,16 +1,16 @@
 <?php
 namespace Service;
 
-use Core\BaseService as BaseService;
-use Core\ProjectInfoParser as ProjectInfoParser;
+use Core\BaseService;
+use Core\Project;
 use Util\FileUtil;
 
 class ListFiles extends BaseService
 {
     public function start() {
-        $projectInfo = ProjectInfoParser::parse($this->options['project']);
-        $projectPath = $projectInfo['projectPath'];
-        $fileExtensions = $projectInfo['fileExtensions'];
+        $project = new Project($this->options['project']);
+        $projectPath = $project->getProjectPath();
+        $fileExtensions = $project->getFileExtensions();
         $fileUtil = new FileUtil();
         $result = $fileUtil->getFileListsContent($projectPath, $fileExtensions);
         return $result;
