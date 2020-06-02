f379c23dffea92cbd51d5fcff28b2e3c2fd4e930
Jim Huang <jim.huang@glu.com>
2020-06-01 19:57:02 -0700

    change localhost to 127.0.0.1

--- config/config.php
+++ config/config.php
@@ -3,7 +3,7 @@ return [
     'dataRoot' => "/usr/local/phim/projects",
     'db' => [
         'type' => 'mysql',
-        'host' => 'localhost',
+        'host' => '127.0.0.1',
         'user' => 'phimide',
         'pass' => '123456',
         'name' => 'phim_ide_db'
